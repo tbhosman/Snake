e70dab842b494c9ecd1ad1bc10a3601cc9138a4b
Tim Hosman <timhosman@hotmail.com>
2015-10-05 13:53:36 +0200

    Werkende player controls gemaakt

--- Snake_Unity/Assets/Scripts/PlayerController.cs
+++ Snake_Unity/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,31 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed;
+    public float rotation_speed;
 
     private Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, speed);
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 rotate = new Vector3(0, moveHorizontal * rotation_speed, 0);   //Use input to rotate player
+        rb.angularVelocity = rotate;
 
-        rb.AddForce(movement * speed);
+        rb.velocity = transform.TransformDirection(new Vector3(speed,0,0));    //Keep speed in local x-direction
     }
 
     void Update()
     {
+        if ((Mathf.Abs(transform.position.x) > 50) || (Mathf.Abs(transform.position.z) > 50))
+        {
+            transform.position = new Vector3(0, 1, 0);
+        }
     }
 }
