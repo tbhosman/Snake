3ebded7a66ecc415193fc654e99fc1acf2975372
Thijs van Baalen <Thijsvanbaalen@hotmail.com>
2015-10-09 13:27:04 +0200

    Camera angle veranderd en fishscripts toegevoegd

---
+++ Snake_Unity/Assets/Scripts/FishBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishBehaviour : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, speed);
+        transform.rotation = Quaternion.AngleAxis(Random.Range(-180.0F, 180.0F), Vector3.up);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = transform.TransformDirection(new Vector3(speed, 0, 0));
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Boundary"))
+        {
+            transform.rotation = Quaternion.AngleAxis(Random.Range(-180.0F, 180.0F), Vector3.up);
+        }
+    }
+}
+
+// Set walls to be collider and trigger AND to be a normal collider, with a kinematic rigid body!
---
+++ Snake_Unity/Assets/Scripts/FishSpawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishSpawn : MonoBehaviour
+{
+    public GameObject Item;
+    private GameObject[] respawns;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 RandomLocation = new Vector3(Random.Range(-50.0F, 50.0F), 1F, Random.Range(-50.0F, 50.0F));
+        Instantiate(Item, RandomLocation, Quaternion.AngleAxis(Random.Range(-180.0F, 180.0F), Vector3.up));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        respawns = GameObject.FindGameObjectsWithTag("Fish");
+        if (respawns == null)
+        {
+            Vector3 RandomLocation = new Vector3(Random.Range(-50.0F, 50.0F), 1F, Random.Range(-50.0F, 50.0F));
+            Instantiate(Item, RandomLocation, Quaternion.AngleAxis(Random.Range(-180.0F, 180.0F), Vector3.up));
+        }
+    }
+}
