b502398e7c358b14f8e81479b5e847e3e9d12b1a
Tim Hosman <timhosman@hotmail.com>
2015-10-12 14:47:04 +0200

    Bubble particles toegevoegd en fog aangepast

---
+++ Snake_Unity/Assets/Scripts/BubbleGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleGenerator : MonoBehaviour {
+
+    public int max_x;
+    public int min_x;
+    public int max_z;
+    public int min_z;
+    public int bubbles;
+    public GameObject bubble_object;
+    public GameObject bubble_parent;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < bubbles; i++)
+        {
+            Vector3 rand_pos = new Vector3(Random.Range(min_x, max_x), 1, Random.Range(min_z, max_z));
+            GameObject Bubble = (GameObject) Instantiate(bubble_object,rand_pos,Quaternion.Euler(new Vector3(-90,0,0)));
+            Bubble.transform.parent = bubble_parent.transform;
+        }	
+	}
+}
