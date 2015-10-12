82edb31bf0a10ecf8a8537fb105c581344340d09
Tim Hosman <timhosman@hotmail.com>
2015-10-12 15:31:48 +0200

    Begin aan UI gemaakt

---
+++ Snake_Unity/Assets/Scripts/GUIController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+    bool paused = false;
+    public GameObject pauseUI;
+
+    void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+             if (paused)
+            {
+                Time.timeScale = 0;
+                pauseUI.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseUI.SetActive(false);
+            }
+        }
+
+        if (paused)
+        {
+            
+        }
+    }
+}
